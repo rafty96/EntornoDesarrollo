9c333ca7eccdc0f0bc125a1db419c35b8513757a
1-DAM <1-DAM@Aula2-PC27>
2016-02-05 10:47:09 +0100

    Primera modificacion

---
+++ src/test.java
@@ -0,0 +1,14 @@
+public class test {
+        public static void main(String[] args) {
+            Condicional1.aprobado(7);
+            Condicional1.aprobado(3);
+            Condicional1.dividir(3, 5);
+            Condicional1.dividir(0, 8);
+            Condicional1.mayor(3, 4);
+            Condicional1.mayor(1, 0);
+            Condicional1.parOImpar(4);
+            Condicional1.parOImpar(3);
+            Condicional1.saldo(100, 500);
+            Condicional1.saldo(1000, 350);
+}
+}
